c509205911da6d35819bae7d59f9af026cedab6c
stv <1>
2021-08-01 23:48:00 +0000

    Added arguments parser. Starting writing adding function

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import os
+import argparse
 
 def platform_presets():
     if os.name == "posix":
-        config_directory = "/home/{}/.config/megasync/".format(os.getlogin())
+        config = "/home/{}/.config/megasync/".format(os.getlogin())
     elif os.name == "nt":
-        config_directory = "C:\\Users\\{}\\AppData\\Local\\MegaSync".format(os.getlogin())
+        config = "C:\\Users\\{}\\AppData\\Local\\MegaSync\\".format(os.getlogin())
+    return config
+
+def add_pair(config): # adds pair
+    pass
+
+# main code
+
+parser = argparse.ArgumentParser(description='MegaSync') # arguments parser
+
+parser.add_argument("-a", action="store_true", help="Add new pairs of folders")
+config = platform_presets()
+args = parser.parse_args()
+if args.a:
+    add_pair()
+
+
+
